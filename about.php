5d9943b90fff3f2a091819a8a737f50f485758e5
Samuel Hilson <samrhilson@gmail.com>
2016-04-06 09:53:30 -0500

    bump version

--- about.php
+++ about.php
@@ -3,7 +3,7 @@
 $ThemeInfo['foundation'] = array(
   'Name'        => 'Foundation',
   'Description' => "An advanced theming boilerplate built on Foundation 5 by ZURB. Geared towards developers and designers who know their way around Sass.",
-  'Version'     => '0.0.2',
+  'Version'     => '0.0.3',
   'Author'      => 'Kasper K. Isager',
   'AuthorEmail' => 'kasperisager@gmail.com',
   'AuthorUrl'   => 'https://github.com/kasperisager',
