c6dcf9ee09305f68af11f7ac3c86c64d6fa5b7a2
R-J <robin.jurinka@gmx.net>
2018-09-27 17:38:29 +0200

    Increase version number

--- about.php
+++ about.php
@@ -5,12 +5,12 @@
 $ThemeInfo['sanmyaku'] = [
   'Name'        => 'Sanmyaku',
   'Description' => "A theme featuring mountains using Zurb Foundation 5",
-  'Version'     => '1.0.4',
+  'Version'     => '1.0.5',
   'Author'      => 'Ryan Jordan, Sam Hilson',
   'AuthorEmail' => 'ryjordan@gmail.com',
   'AuthorUrl'   => 'https://github.com/codesequence',
   'License'     => 'MIT',
-  'RequiredApplications' => ['Vanilla' => '2.2.x'],
+  'RequiredApplications' => ['Vanilla' => '2.6'],
 
   'Options' => [
     'Styles' => [
